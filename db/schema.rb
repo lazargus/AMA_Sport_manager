47a15d3287ae960a84208198d0c2fca3441e235d
benoit barges <benoitbarges@gmail.com>
2020-05-26 17:04:10 +0200

    geocoder install, latitude longitude to tournaments, geocoded by address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_142502) do
+ActiveRecord::Schema.define(version: 2020_05_26_144403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2020_05_26_142502) do
     t.string "official_link"
     t.text "description"
     t.string "last_winner"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
