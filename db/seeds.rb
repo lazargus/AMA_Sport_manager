265b2c2da67abd28266071c3f06eb90d2d0ea909
Francois <francois.desazars@gmail.com>
2020-05-27 12:54:22 +0200

    finished seed until ajax pb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
+
+def download_to_file(uri)
+  stream = open(uri, "rb")
+  return stream if stream.respond_to?(:path)
+  Tempfile.new.tap do |file|
+    file.binmode
+    IO.copy_stream(stream, file)
+    stream.close
+    file.rewind
+  end
+end
 
 url_list = "https://www.atptour.com/en/scores/results-archive"
 url_tournaments = []
@@ -9,67 +18,129 @@ url_tournaments = []
 html_file = open(url_list).read
 html_doc = Nokogiri::HTML(html_file)
 
+
+
 html_doc.search('.tourney-result .title-content a').each do |element|
   name_path = element.attribute('href').value
   tournament_link = "https://www.atptour.com#{name_path}?detailTab=points"
   url_tournaments << tournament_link
 end
 
-url_tournaments.first(2).each do |url|
+# url_tournaments[44]
+# url_tournaments[54]
+
+puts "Cleaning tournaments...."
+Tournament.destroy_all
+
+puts "Creating tournament database...."
+url_tournaments.each do |url|
   html_file = open(url).read
   html_doc = Nokogiri::HTML(html_file)
+  p "------------------------"
+
   # tournament name
-  name = html_doc.at('.last-name').text
+  if html_doc.at('.last-name').present?
+    name = html_doc.at('.last-name').text.strip
+  else
+    name = ""
+  end
+
   # total prize money
-  prize_money = html_doc.at('.prize-money-bottom').text.strip
+  if html_doc.at('.prize-money-bottom').present?
+    prize_money = html_doc.at('.prize-money-bottom').text.strip
+  else
+    prize_money = 0
+  end
   # tournament surface
-  surface = html_doc.at('.surface-bottom .item-value').text.strip
+  if html_doc.at('.surface-bottom .item-value').present?
+    surface = html_doc.at('.surface-bottom .item-value').text.strip
+  else
+    surface = ""
+  end
   # total single participants
-  total_participants = html_doc.at('.bracket-sgl .item-inline-value').text.strip
+  if html_doc.at('.bracket-sgl .item-inline-value').present?
+    total_participants = html_doc.at('.bracket-sgl .item-inline-value').text.strip
+  else
+    total_participants = 28
+  end
   # link to official website
-  official_link = html_doc.at('.official-website-link').attribute('href').value
+  if html_doc.at('.player-profile-hero-social .official-website-link').present?
+    official_link = html_doc.at('.player-profile-hero-social .official-website-link').attribute('href').value
+  else
+    official_link = ""
+  end
   # ATP description
-  description = html_doc.at('.the-blurb').text.strip
+  if html_doc.at('.the-blurb').present?
+    description = html_doc.at('.the-blurb').text.strip
+  else
+    description = ""
+  end
   # location of the tournament
-  address = html_doc.search('.hero-date-range')[0].children.first.text
+  if html_doc.search('.hero-date-range').present?
+    address = html_doc.search('.hero-date-range')[0].children.first.text
+  else
+    address = ""
+  end
   # start and end dates
-  date = html_doc.search('.hero-date-range')[1].children.first.text.gsub("- ", "").split(" ")
-  start_date = Date.parse([date[0], date[1], date[3]].join(" "))
-  end_date = Date.parse([date[0], date[2], date[3]].join(" "))
+  if html_doc.search('.hero-date-range').present?
+    date = html_doc.search('.hero-date-range')[1].children.first.text.gsub("- ", "").split(" ")
+    start_date = Date.parse([date[0], date[1], date[3]].join(" "))
+    end_date = Date.parse([date[0], date[2], date[3]].join(" "))
+  else
+    start_date = 2020/01/01
+    start_end = 2020/01/02
+  end
   # lon and lat
-  gps_coordinates = html_doc.search('.tournament-hero-links').children[2].attributes.first[1].value.split('/')[5].split(',')
-  lat = gps_coordinates[0]
-  lon = gps_coordinates[1]
+  if html_doc.search('.tournament-hero-links').present?
+    gps_coordinates = html_doc.search('.tournament-hero-links').children[2].attributes.first[1].value.split('/')[5].split(',')
+    lat = gps_coordinates[0]
+    lon = gps_coordinates[1]
+  else
+    lat = 48.8588376
+    lon = 2.2768487
+  end
   # tournament photo
   tournament_photo = html_doc.at('#tournamentHero')
   photo_link = "https://www.atptour.com/#{tournament_photo.attributes['style'].value.split("'")[1]}"
+  downloaded_photo = URI.open(photo_link)
+  if downloaded_photo.is_a? StringIO
+    downloaded_photo = download_to_file(URI.parse(photo_link))
+  end
+
   # atp logo
   tournament_logo = html_doc.at('.tournmanet-logo')
   logo_link = "https://www.atptour.com#{tournament_logo.children[1].attributes['src'].value}"
+  downloaded_logo = URI.open(logo_link)
+  if downloaded_logo.is_a? StringIO
+    downloaded_logo = download_to_file(URI.parse(logo_link))
+  end
 
   # atp category
-  category = html_doc.at('.tournmanet-logo').children[1].attributes['alt'].value
+  if html_doc.at('.tournmanet-logo').present?
+    category = html_doc.at('.tournmanet-logo').children[1].attributes['alt'].value
+  else
+    category = ""
+  end
 
-  tournament = Tournament.new({ name: name, address: address, prize_money: prize_money, surface: surface, category: category, start_date: start_date, end_date: end_date, participants: total_participants, description: description, latitude: lat, longitude: lon,  })
-  tournament.photo.attach(io: URI.open(photo_link), filename: "tournament.jpg", content_type: "image/jpg")
-  tournament.logo.attach(io: URI.open(logo_link), filename: "category.jpg", content_type: "image/jpg")
-  tournament.save!
+  # atp points
+  # tournament_points = html_doc.search('#tourneyOverviewTabs > div.tourney-tab-content > div > div:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(3)')
+
+  # tournament creation
+  tournament = Tournament.new({ name: name, address: address, official_link: official_link, prize_money: prize_money, surface: surface, category: category, start_date: start_date, end_date: end_date, participants: total_participants, description: description, latitude: lat, longitude: lon })
+  tournament.save
+  tournament.photo.attach(io: downloaded_photo, filename: "tournament.jpg")
+  tournament.logo.attach(io: downloaded_logo, filename: "category.png")
+
+  p "#{name} created!"
 end
 
-# puts "Cleaning Users seeds"
-# User.destroy_all
-# puts "Creating some Users"
-# benoit = User.new({ first_name: "Benoit", last_name: "Bargès", email: "benoit@gmail.com", password: "tennis" })
-# benoit.avatar.attach(io: File.open("app/assets/images/avatars/benoit.jpg"), filename: "avatar.jpg", content_type: "image/jpg")
-# benoit.save
-# augustin = User.new({ first_name: "Augustin", last_name: "Lazarus", email: "augustin@gmail.com", password: "tennis" })
-# augustin.avatar.attach(io: File.open("app/assets/images/avatars/augustin.jpg"), filename: "avatar.jpg", content_type: "image/jpg")
-# augustin.save
-# francois = User.new({ first_name: "François", last_name: "Desazars", email: "francois@gmail.com", password: "tennis" })
-# francois.avatar.attach(io: File.open("app/assets/images/avatars/francois.jpg"), filename: "avatar.jpg", content_type: "image/jpg")
-# francois.save
-# alexandre = User.new({ first_name: "Alexandre", last_name: "Deniau", email: "alexandre@gmail.com", password: "tennis" })
-# alexandre.avatar.attach(io: File.open("app/assets/images/avatars/alex.jpg"), filename: "avatar.jpg", content_type: "image/jpg")
-# alexandre.save
-# puts "Finished!"
+puts "Finished"
+
+puts "Cleaning Users seeds"
+User.destroy_all
+puts "Creating some Users"
+stephane = User.new({ first_name: "stephane", last_name: "Bargès", email: "stephane@gmail.com", password: "tennis" })
+stephane.avatar.attach(io: File.open("app/assets/images/avatars/stephane.jpg"), filename: "avatar.jpg", content_type: "image/jpg")
+stephane.save
+puts "Finished!"
 
