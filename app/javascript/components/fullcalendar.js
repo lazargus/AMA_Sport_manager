25c98664d8b8202a6453f75369d5a8c5edf062db
benoit barges <benoitbarges@gmail.com>
2020-05-28 16:47:34 +0200

    some js

--- app/javascript/components/fullcalendar.js
+++ app/javascript/components/fullcalendar.js
@@ -3,18 +3,36 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 
-
-const fullcalendar = () => {
-
-    const calendarEl = document.getElementById('calendar');
-
-    const calendar = new Calendar(calendarEl, {
-      plugins: [ dayGridPlugin ]
-    });
-    console.log("hello")
-    calendar.render();
+const getEvents = () => {
+  const tournamentCards = document.querySelectorAll('.my-tournaments');
+  const events = [...tournamentCards].map(card => {
+    return {
+      title:card.dataset.tournamentName,
+      start:card.dataset.startDate,
+      end:card.dataset.endDate
+    }
+  });
+  return events;
 }
 
+const fullcalendar = () => {
+  const calendarEl = document.getElementById('calendar');
 
+  const calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin ],
+    events: getEvents(),
+    eventClick: function(info) {
+      const modal = document.getElementById("myModal");
+      const close = document.querySelector(".close");
+      modal.style.display = "block";
+      close.addEventListener("click", (e) => {
+        modal.style.display = "none";
+      });
+    }
+
+  });
+  calendar.render();
+  calendar.getEvents();
+}
 
 export { fullcalendar };
