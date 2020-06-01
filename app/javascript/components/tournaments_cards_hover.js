a46274780081021c2ce0a2e4c6c8d7196b4b8201
benoit barges <benoitbarges@gmail.com>
2020-06-01 18:16:12 +0200

    manage btn

--- app/javascript/components/tournaments_cards_hover.js
+++ app/javascript/components/tournaments_cards_hover.js
@@ -4,7 +4,7 @@ const tournamentsCardsHover = () => {
   tournamentCards.forEach((card) => {
     card.addEventListener('mouseover', (e) => {
       const imgUrl = card.dataset.imgUrl;
-      card.style.backgroundImage = `linear-gradient(142deg, rgba(3,56,96,1) 0%, rgba(3,56,96,0.8897934173669468) 50%, rgba(3,56,96,0.6685049019607843) 100%), url(${imgUrl})`;
+      card.style.backgroundImage = `linear-gradient(153deg, rgba(3,56,96,1) 0%, rgba(5,80,136,0.8057598039215687) 50%, rgba(20,101,163,0.8169642857142857) 100%), url(${imgUrl})`;
     });
 
     card.addEventListener('mouseout', (e) => {
