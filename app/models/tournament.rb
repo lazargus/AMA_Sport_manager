229935d99a4489ea22adb771d02e190ee361d355
Francois <francois.desazars@gmail.com>
2020-06-02 13:17:08 +0200

    finished qualifier seed

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,6 +5,7 @@ class Tournament < ApplicationRecord
   has_many :expenses, dependent: :destroy
   has_many :earnings, dependent: :destroy
 
+  before_create :data_for_qualifiers
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
@@ -22,4 +23,23 @@ class Tournament < ApplicationRecord
   # def find_user_earning
   #   Earning.find_by(tournament/tournament_id)
   # end
+
+  def data_for_qualifiers
+    @tournament_data = JSON.parse(self.data)
+    if @tournament_data['Qualifier 2']
+      @q2 = @tournament_data['Qualifier 2']['prize_money']
+      if @tournament_data['Qualifier']['prize_money'] == 0
+        @tournament_data['Qualifier']['prize_money'] = @q2 * 2
+        @tournament_data['Qualifier 3']['prize_money'] = @q2 * 2
+        self.data = @tournament_data.to_json
+      end
+    end
+
+    # sur Qualifier et Qualifier 3
+    # pour chaque pair verifier la valeur de prize money
+    # si prize money = 0
+    # assigner nouvelle valeur a prize money (/10?)
+    # update instance du tournoi avec nouvelles valeurs de q1
+    # Reparser en json (tournoi.update(data.to_json))
+  end
 end
