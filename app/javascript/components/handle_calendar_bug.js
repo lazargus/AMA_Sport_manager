5ac95c376ce82738eeaa3f35fd55b766bf615f91
benoit barges <benoitbarges@gmail.com>
2020-06-03 09:23:52 +0200

    handle map bug

--- app/javascript/components/handle_calendar_bug.js
+++ app/javascript/components/handle_calendar_bug.js
@@ -1,11 +1,7 @@
 import { fullcalendar } from "./fullcalendar";
 
 const handleCalendarBug = () => {
-  // const prevBtn = document.querySelector('.fc-prev-button');
-  // const nextBtn = document.querySelector('.fc-next-button');
   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    // prevBtn.click();
-    // nextBtn.click();
     fullcalendar();
   })
 }
