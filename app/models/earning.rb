5c552b53c1bca075c70fe4bfd98da08fa7e61835
Francois <francois.desazars@gmail.com>
2020-06-03 18:46:01 +0200

    finished

--- app/models/earning.rb
+++ app/models/earning.rb
@@ -6,4 +6,13 @@ class Earning < ApplicationRecord
   validates :category, presence: true
   validates :category, inclusion: { in: CATEGORY }
 
+  def collection_for_simple_form
+    return [] if tournament.nil?
+    result = []
+    JSON.parse(tournament.data).each_pair do |k, v|
+      result << ["#{k}: $#{v["prize_money"]}", v["prize_money"]]
+    end
+    return result
+  end
+
 end
